edb3aea1dc96c74e4f7545a24b3af998a13b0f63
Shakil Anwar <shakilanwar1998@gmail.com>
2024-04-12 15:02:37 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/',function(){
+    return response([
+        'message' => "Welcome to eBay API services module"
+    ]);
+});
+
 Route::get('/auth',[\App\Http\Controllers\AuthenticationController::class,'handleRedirect']);
+
